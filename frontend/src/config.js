bae08f3e7ab25e91bb19aeaf5daeb47ccce73bd1
portzot-derech <idanahum1122@gmail.com>
2026-04-18 13:20:47 +0300

    Refactor API URL in frontend configuration to use a constant for improved maintainability and clarity.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
+const API_URL = 'https://script.google.com/macros/s/AKfycbxvBltVlROk0KoTDwMBtL6QSjaKpZ5wuVYpR4B5KSFi0VBjheu3At9uyATMvP4oAUzY/exec';
+
 export const config = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycby_Sf9TZlj2fi1xtfeZ5nzBdE7Fz1a_0pHiVcVoXwnajnJiAKhvibh6dSihOsY148EE/exec'
+  apiUrl: API_URL
 };
