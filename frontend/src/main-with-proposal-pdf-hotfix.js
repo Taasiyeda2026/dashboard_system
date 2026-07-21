8a902d34afa8ab688cc2fa0242034de3d53020ca
Idan Nahum <idanahum1122@gmail.com>
2026-07-21 23:19:23 +0300

    Load proposal PDF download filename hotfix

--- frontend/src/main-with-proposal-pdf-hotfix.js
+++ frontend/src/main-with-proposal-pdf-hotfix.js
@@ -1,4 +1,5 @@
 import './proposal-pdf-svg-origin-clean.js';
 import './proposal-pdf-storage-key-hotfix.js';
+import './proposal-pdf-download-filename-hotfix.js';
 import './client-contact-persistence-hotfix.js';
 import './main.js';
