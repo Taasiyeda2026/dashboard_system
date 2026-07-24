177667e394f38e5f95e266e84656508bcacf9754
Idan Nahum <idanahum1122@gmail.com>
2026-07-25 02:39:40 +0300

    Emergency rollback annual review click guard

--- frontend/src/main-with-proposal-pdf-hotfix.js
+++ frontend/src/main-with-proposal-pdf-hotfix.js
@@ -1,4 +1,3 @@
-import './annual-reviews-open-button-guard.js';
 import './proposal-pdf-svg-origin-clean.js';
 import './proposal-pdf-storage-key-hotfix.js';
 import './proposal-pdf-download-filename-hotfix.js';
