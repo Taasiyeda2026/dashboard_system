d47cd78a9ac0d4ebfaf6bc2490dc547c2ad3a0ab
Idan Nahum <idanahum1122@gmail.com>
2026-08-11 15:35:31 +0300

    test: remove temporary client-file CI wrapper

--- tests/client-file-unified-screen-ci.test.mjs
+++
@@ -1,8 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM('', { url: 'http://localhost/' });
-
-globalThis.localStorage = dom.window.localStorage;
-globalThis.sessionStorage = dom.window.sessionStorage;
-
-await import('./client-file-unified-screen.test.mjs');
