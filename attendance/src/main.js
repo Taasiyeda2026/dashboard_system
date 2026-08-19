08f860184e708fb8e663a51ab98b78583fafd29d
Idan Nahum <idanahum1122@gmail.com>
2026-08-19 04:25:49 +0300

    Redesign attendance UX for round 3

--- attendance/src/main.js
+++ attendance/src/main.js
@@ -1,5 +1,7 @@
 import { startApp } from './app.js';
+import { initRound3Ui } from './round3-ui.js';
 import { registerServiceWorker } from './services/sw-registration.service.js';
 
 startApp(document.getElementById('app'), document.getElementById('bottom-nav'));
+initRound3Ui(document.getElementById('app'));
 registerServiceWorker();
