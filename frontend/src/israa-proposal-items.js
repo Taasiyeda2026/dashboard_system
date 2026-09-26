20170e67f94b12cbee4c7f53f1ed3b5e0e26b9e1
Idan Nahum <idanahum1122@gmail.com>
2026-09-26 22:19:17 +0300

    Bust Israa partial-share workspace cache

--- frontend/src/israa-proposal-items.js
+++ frontend/src/israa-proposal-items.js
@@ -26,7 +26,7 @@ function ensureMainActivitiesWorkspace() {
   if (!workspaceImportPromise) {
     workspaceImportPromise = Promise.all([
       import('./activities-approved-ui-fix.js?v=20260831-israa-v1'),
-      import('./israa-activities-main-workspace.js?v=20260926-v6')
+      import('./israa-activities-main-workspace.js?v=20260926-v7')
     ])
       .catch((error) => {
         workspaceImportPromise = null;
